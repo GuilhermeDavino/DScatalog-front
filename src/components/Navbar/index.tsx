0362e28c3664714df0956d02282e60f4091d442c
Guilherme <guilherme.1009@hotmail.com>
2025-07-15 11:58:04 -0300

    navbar styles

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,16 +1,18 @@
+import './styles.css';
+
 const Navbar = () => {
     return (
         <>
-            <nav className="bg-primary">
-                <div className='container'>
+            <nav className="navbar navbar-expand-md bg-primary main-nav">
+                <div className='container-fluid'>
                     <div>
-                        <a href="#"><h4>DS Catalog</h4></a>
+                        <a href="link" className="nav-logo-text"><h4>DS Catalog</h4></a>
                     </div>
-                    <div className='container-list'>
-                        <ul>
-                            <li><a href="#">Home</a></li>
-                            <li><a href="#">CATALÓGO</a></li>
-                            <li><a href="#">ADMIN</a></li>
+                    <div className='collapse navbar-collapse'>
+                        <ul className='navbar-nav offset-md-2 main-menu'>
+                            <li><a href="link" className='active'>Home</a></li>
+                            <li><a href="link">CATALÓGO</a></li>
+                            <li><a href="link">ADMIN</a></li>
                         </ul>
                     </div>
                 </div>
